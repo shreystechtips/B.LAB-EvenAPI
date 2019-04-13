948ecc7ea01b1b6937cf498c03bad25491c8c6fb
Shrey Aeron <shreystechtips@gmail.com>
2019-04-13 13:53:15 -0700

    Added Xamarin Code

---
+++ BuyingAssistant/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Plugin.Clipboard;
+using Xamarin.Forms;
+using System.Text;
+using System.Net;
+
+
+namespace BuyingAssistant
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
