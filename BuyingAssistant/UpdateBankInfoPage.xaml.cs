57863ff812072b13506c61dea0c7c7d8bd7a7a46
ForeverKamodo <scout.trooper13@gmail.com>
2019-04-13 18:59:11 -0700

    test

--- BuyingAssistant/UpdateBankInfoPage.xaml.cs
+++ BuyingAssistant/UpdateBankInfoPage.xaml.cs
@@ -5,46 +5,43 @@ using Xamarin.Essentials;
 
 namespace BuyingAssistant {
 
-    using System.IO;
-    using System.Net;
-    using System.Text;
     using System;
+    using System.IO;
     using Plugin.Clipboard;
     using Xamarin.Forms;
+    using System.Text;
+    using System.Net;
 
     //Searching for a loan, user enters the product they want and its costs
     public partial class UpdateBankInfoPage : ContentPage {
 
-        public UpdateBankInfoPage () {
-            InitializeComponent ();
-            init ();
+        public UpdateBankInfoPage() {
+            InitializeComponent();
+            init();
         }
 
-        async private void init () {
-
-            Device.BeginInvokeOnMainThread (async () => {
-                await DisplayAlert ("Alert", "Bad API Call", "Ok");
-            });
+        async private void init() {
+          
         }
 
         //this method is called when the button with the text "click here" is clicked (i called the method this, you can rename this and the clicked parameter in the .xaml file)
-        void Handle_Clicked (object sender, System.EventArgs e) {
+        void Handle_Clicked(object sender, System.EventArgs e) {
 
-            Device.BeginInvokeOnMainThread (async () => {
-                await DisplayAlert ("Alert", "Hi", "Ok");
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Alert", "Hi", "Ok");
             });
         }
 
-        private void Entry_TextChanged (object sender, TextChangedEventArgs e) {
-
-            Preferences.Set ("PersonName", e.NewTextValue);
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e) {
+            Preferences.Set("PersonName", e.NewTextValue);
         }
 
-        private void CardBenefits_SelectedIndexChanged (System.Object sender, System.EventArgs e) {
+        private void CardBenefits_SelectedIndexChanged(System.Object sender, System.EventArgs e) {
 
         }
 
-        private void _SelectedIndexChanged (System.Object sender, System.EventArgs e) {
+        private void _SelectedIndexChanged(System.Object sender, System.EventArgs e) {
 
         }
     }
