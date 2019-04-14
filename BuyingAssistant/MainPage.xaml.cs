2c071f813619b8494ec416b77ea71e37c37c397a
ForeverKamodo <scout.trooper13@gmail.com>
2019-04-13 22:37:49 -0700

    Reformatted

--- BuyingAssistant/MainPage.xaml.cs
+++ BuyingAssistant/MainPage.xaml.cs
@@ -11,29 +11,24 @@ using System.Collections.Generic;
 
 namespace BuyingAssistant {
     //Searching for a loan, user enters the product they want and its costs
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
             InitializeComponent();
             init();
             populateList();
         }
 
-        public void populateList()
-        {
-            JObject s =  JObject.Parse(Preferences.Get("savedOffers", "{}"));
+        public void populateList() {
+            JObject s = JObject.Parse(Preferences.Get("savedOffers", "{}"));
             Dictionary<String, String> vals = new Dictionary<string, string>();
-            for(int i = 0; i < s.Count; i++)
-            {
-                JArray temp = (JArray) s[i];
-                vals.Add ((string) temp["key"],(string) temp["val"]);
+            for (int i = 0; i < s.Count; i++) {
+                JArray temp = (JArray)s[i]; 
+                vals.Add((string)temp["key"], (string)temp["val"]);
             }
             savedList.ItemsSource = vals;
         }
 
-        async private void init()
-        {
+        async private void init() {
             //tries to create the web request (it doesn't work right now, we need to research how to do this)
             /*
             BankBalance.Text = "trying";        
@@ -51,40 +46,34 @@ namespace BuyingAssistant {
                 ds.Write(byteArray, 0, byteArray.Length);
                 BankBalance.Text = "write req";
                 ds.Close();
-                */               
+                */
 
-var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.evenfinancial.com/leads/rateTables");
-httpWebRequest.ContentType = "application/json";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.evenfinancial.com/leads/rateTables");
+            httpWebRequest.ContentType = "application/json";
             httpWebRequest.Headers["Authorization"] = "Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2";
-httpWebRequest.Method = "POST";
+            httpWebRequest.Method = "POST";
 
-using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-{
-    string json = "{\"user\":\"test\"," +
-                  "\"password\":\"bla\"}";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream())) {
+                string json = "{\"user\":\"test\"," +
+                              "\"password\":\"bla\"}";
 
-    streamWriter.Write(json);
-    streamWriter.Flush();
-    streamWriter.Close();
-}
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
             WebResponse httpResponse;
             String j;
-            try
-            {
+            try {
                 httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                {
-                     j = streamReader.ReadToEnd();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) {
+                    j = streamReader.ReadToEnd();
 
                 }
-            }
-            catch
-            {
+            } catch {
                 Console.WriteLine("error 401");
                 j = "ere";
             }
-            Device.BeginInvokeOnMainThread(() =>
-            {
+            Device.BeginInvokeOnMainThread(() => {
                 DisplayAlert("Alert", j, "Ok");
                 Console.WriteLine(j);
             });
@@ -110,17 +99,14 @@ using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
         }
 
         //this method is called when the button with the text "click here" is clicked (i called the method this, you can rename this and the clicked parameter in the .xaml file)
-        void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            Device.BeginInvokeOnMainThread(async () =>
-            {
+        void Handle_Clicked(object sender, System.EventArgs e) {
+            Device.BeginInvokeOnMainThread(async () => {
                 await DisplayAlert("Alert", "Hi", "Ok");
             });
         }
 
-        void SearchTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
-        {
-            
+        void SearchTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
+
         }
     }
 }
