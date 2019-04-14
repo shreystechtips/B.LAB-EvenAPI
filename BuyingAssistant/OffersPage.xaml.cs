afc3c70e0b8fccc77817f85e65984f2721cde0da
Shrey Aeron <shreystechtips@gmail.com>
2019-04-13 22:45:24 -0700

    Create Framework for Offers Page

---
+++ BuyingAssistant/OffersPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BuyingAssistant
+{
+    public partial class OffersPage : ContentPage
+    {
+        public OffersPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
