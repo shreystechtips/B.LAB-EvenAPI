bd702f5a8b84b31ffc5a4b5ef65e4f3405fd4e78
ForeverKamodo <scout.trooper13@gmail.com>
2019-04-14 01:19:59 -0700

    Finished adding required feels
    We need to hard code randomized birthdays, emails, and social security numbers

--- BuyingAssistant/UpdateBankInfoPage.xaml.cs
+++ BuyingAssistant/UpdateBankInfoPage.xaml.cs
@@ -15,15 +15,17 @@ namespace BuyingAssistant {
         public UpdateBankInfoPage(Boolean isNextVisible) {
             InitializeComponent();
             registerButton.IsVisible = isNextVisible;
-            setFields();
+            SetFields();
         }
 
-        void setFields(){
-            PersonName.Text = Preferences.Get("PersonName", "");
+        void SetFields() {
+            FirstName.Text = Preferences.Get("FirstName", "");
+            LastName.Text = Preferences.Get("LastName", "");
+            AnnualIncome.Text = Preferences.Get("AnnualIncome", "");
             CardBenefits.SelectedIndex = Preferences.Get("CardBenefits", -1);
             CreditRange.SelectedIndex = Preferences.Get("CreditRange", -1);
             TypeOfAccount.SelectedIndex = Preferences.Get("TypeOfAccount", -1);
-            PaymentRate.SelectedIndex = Preferences.Get("PaymentRate", -1);
+            PaymentFrequency.SelectedIndex = Preferences.Get("PaymentFrequency", -1);
             CurrentEmploymentStatus.SelectedIndex = Preferences.Get("CurrentEmploymentStatus", -1);
             FinanceOfResidence.SelectedIndex = Preferences.Get("FinanceOfResidence", -1);
             ResidenceType.SelectedIndex = Preferences.Get("ResidenceType", -1);
@@ -34,17 +36,20 @@ namespace BuyingAssistant {
 
         public static String alert = "";
 
-        void createAlert()
-        {
-            if (PersonName.Text.Equals(""))
-                alert += "\n- Person Name";
+        void CreateAlert() {
+            if (FirstName.Text.Equals(""))
+                alert += "\n- First Name";
+            if (LastName.Text.Equals(""))
+                alert += "\n- Last Name";
+            if (AnnualIncome.Text.Equals(""))
+                alert += "\n- Annual Income";
             if (CardBenefits.SelectedIndex == -1)
                 alert += "\n- Card Benefits";
             if (CreditRange.SelectedIndex == -1)
                 alert += "\n- Credit Range";
             if (TypeOfAccount.SelectedIndex == -1)
                 alert += "\n- Type of Account";
-            if (PaymentRate.SelectedIndex == -1)
+            if (PaymentFrequency.SelectedIndex == -1)
                 alert += "\n- Payment Rate";
             if (CurrentEmploymentStatus.SelectedIndex == -1)
                 alert += "\n- Current Employment Status";
@@ -53,73 +58,73 @@ namespace BuyingAssistant {
             if (ResidenceType.SelectedIndex == -1)
                 alert += "\n- Residence Type";
             if (ReasonForLoan.SelectedIndex == -1)
-                alert += "\n- Reason for loan";
+                alert += "\n- Reason for Loan";
             if (HighestEducationalDegree.SelectedIndex == -1)
                 alert += "\n- Highest Education Degree";
             if (TypeOfReturnOfferWanted.SelectedIndex == -1)
-                alert += "\n- Type of return offer wanted";
-        }
-        void Entry_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
-            Preferences.Set("PersonName", e.NewTextValue);
+                alert += "\n- Type of Return Offer Wanted";
         }
 
-         void CardBenefits_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void FirstName_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
+            Preferences.Set("FirstName", e.NewTextValue);
+        }
+
+        void LastName_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
+            Preferences.Set("LastName", e.NewTextValue);
+        }
+
+        void AnnualIncome_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e) {
+            Preferences.Set("AnnualIncome", e.NewTextValue);
+        }
+
+        void CardBenefits_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("CardBenefits", CardBenefits.SelectedIndex);
         }
 
-         void CreditRange_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void CreditRange_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("CreditRange", CreditRange.SelectedIndex);
         }
 
-         void TypeOfAccount_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void TypeOfAccount_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("TypeOfAccount", TypeOfAccount.SelectedIndex);
         }
 
-         void PaymentRate_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
-            Preferences.Set("PaymentRate", PaymentRate.SelectedIndex);
+        void PaymentFrequency_SelectedIndexChanged(object sender, System.EventArgs e) {
+            Preferences.Set("PaymentFrequency", PaymentFrequency.SelectedIndex);
         }
 
-         void CurrentEmploymentStatus_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void CurrentEmploymentStatus_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("CurrentEmploymentStatus", CurrentEmploymentStatus.SelectedIndex);
         }
 
-         void FinanceOfResidence_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void FinanceOfResidence_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("FinanceOfResidence", FinanceOfResidence.SelectedIndex);
         }
 
-         void ResidenceType_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void ResidenceType_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("ResidenceType", ResidenceType.SelectedIndex);
         }
 
-         void ReasonForLoan_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void ReasonForLoan_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("ReasonForLoan", ReasonForLoan.SelectedIndex);
         }
 
-         void HighestEducationalDegree_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void HighestEducationalDegree_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("HighestEducationalDegree", HighestEducationalDegree.SelectedIndex);
         }
 
-         void TypeOfReturnOfferWanted_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
+        void TypeOfReturnOfferWanted_SelectedIndexChanged(object sender, System.EventArgs e) {
             Preferences.Set("TypeOfReturnOfferWanted", TypeOfReturnOfferWanted.SelectedIndex);
         }
 
-        async void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            if (PersonName.Text.Equals("") ||
+        async void Handle_Clicked(object sender, System.EventArgs e) {
+            if (FirstName.Text.Equals("") ||
+                LastName.Text.Equals("") ||
+                AnnualIncome.Text.Equals("") ||
                 CardBenefits.SelectedIndex == -1 ||
                 CreditRange.SelectedIndex == -1 ||
                 TypeOfAccount.SelectedIndex == -1 ||
-                PaymentRate.SelectedIndex == -1 ||
+                PaymentFrequency.SelectedIndex == -1 ||
                 CurrentEmploymentStatus.SelectedIndex == -1 ||
                 FinanceOfResidence.SelectedIndex == -1 ||
                 ResidenceType.SelectedIndex == -1 ||
@@ -127,7 +132,7 @@ namespace BuyingAssistant {
                 HighestEducationalDegree.SelectedIndex == -1 ||
                 TypeOfReturnOfferWanted.SelectedIndex == -1)
             {
-                createAlert();
+                CreateAlert();
                 await DisplayAlert("Please fill out the following:", alert, "OK");
             }
             await Navigation.PushAsync(new MainTabbedLayout());
