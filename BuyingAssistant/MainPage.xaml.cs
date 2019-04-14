63d234ec79293094fa9db035bcf8c5ca69daffa9
Shrey Aeron <shreystechtips@gmail.com>
2019-04-13 19:57:34 -0700

    Got Responses to work for pending offers (can easily adapt for other types of datas)

--- BuyingAssistant/MainPage.xaml.cs
+++ BuyingAssistant/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Essentials;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace BuyingAssistant {
     //Searching for a loan, user enters the product they want and its costs
@@ -35,28 +36,10 @@ namespace BuyingAssistant {
         async private void init()
         {
             //tries to create the web request (it doesn't work right now, we need to research how to do this)
-            /*
-            BankBalance.Text = "trying";        
-           JObject data = new JObject();
-
-            WebRequest req = WebRequest.Create("https://api.evenfinancial.com/leads/rateTables");
-                req.Method = "POST";
-            req.ContentType = "application/json";
-            BankBalance.Text = "created req";
-                string postData = "";//"api_option=" + "paste" + "&api_paste_code=" + temp + "&api_dev_key=" + "add_api_key";
-                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-                req.ContentLength = byteArray.Length;
-
-                Stream ds = req.GetRequestStream();
-                ds.Write(byteArray, 0, byteArray.Length);
-                BankBalance.Text = "write req";
-                ds.Close();
-                */               
-
-var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.evenfinancial.com/leads/rateTables");
-httpWebRequest.ContentType = "application/json";
+           var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.evenfinancial.com/leads/rateTables");
+            httpWebRequest.ContentType = "application/json";
             httpWebRequest.Headers["Authorization"] = "Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2";
-httpWebRequest.Method = "POST";
+            httpWebRequest.Method = "POST";
 
 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
 {
@@ -75,38 +58,28 @@ using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
                      j = streamReader.ReadToEnd();
-
+                }
+                await Clipboard.SetTextAsync(j);
+                BankBalance.FontSize = 15;
+                JObject returnData = JObject.Parse(j);
+                BankBalance.FontSize = 15;
+                JArray pending = (JArray)returnData["pendingResponses"];
+                Dictionary<String, String> vals = new Dictionary<string, string>();
+                for (int i = 0; i < pending.Count; i++)
+                {
+                     JObject temp = (JObject) pending[i];
+                    JObject temp2 = (JObject)temp["partner"];
+                    BankBalance.Text += temp2["name"];
                 }
             }
-            catch
+            catch (HttpRequestException)
             {
-                Console.WriteLine("error 401");
-                j = "ere";
+                BankBalance.Text = "oof";
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    DisplayAlert("Alert", "Request Error", "Ok");
+                });
             }
-            Device.BeginInvokeOnMainThread(() =>
-            {
-                DisplayAlert("Alert", j, "Ok");
-                Console.WriteLine(j);
-            });
-            /*
-                    try
-                        {
-                            WebResponse wr = req.GetResponse();
-                            ds = wr.GetResponseStream();
-                            StreamReader reader = new StreamReader(ds);
-
-                            String ret = await reader.ReadToEndAsync();
-                            CrossClipboard.Current.SetText(ret);
-                            await DisplayAlert("Success", ret, "OK");
-                        }
-                        catch
-                        {
-                            Device.BeginInvokeOnMainThread(async () =>
-                            {
-                                await DisplayAlert("Alert", "Bad API Call", "Ok");
-                            });
-                        }
-                        */
         }
 
         //this method is called when the button with the text "click here" is clicked (i called the method this, you can rename this and the clicked parameter in the .xaml file)
